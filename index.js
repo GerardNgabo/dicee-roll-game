a503efd546a843f031f3a3ab8dfb9f180e361d6c
Gerard Ngabo <ngabongwi5@hotmail.com>
2022-07-19 20:09:32 +0200

    add Roll Button

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let randomNumber1 = Math.floor(Math.random() * 6 + 1);
+function rollDicee() {
+  let randomNumber1 = Math.floor(Math.random() * 6 + 1);
 let randomNumber2 = Math.floor(Math.random() * 6 + 1);
 
 document
@@ -15,4 +16,6 @@ if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerText = "Player 2 Wins 🚩";
 } else {
   document.querySelector("h1").innerText = "🚩 It's a Draw 🚩";
+}
+
 }
